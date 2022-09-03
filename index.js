ef00f3d7e0f358518aa33e8867ac39c51e7fff71
Anne Ogongo <anneogongo98@gmail.com>
2022-09-03 18:51:49 +0300

    More

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const EMPTY_HEART = '♡'
 
 const get = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
 
+ const rem =  function removeNull(get) {
+    return get.filter(n => n !== null )
+  }
+  console.log(rem)
+
+
 function liking() {
   array.forEach((item => (item.addEventListener('click', () => {
   if (item.innerHTML === EMPTY_HEART) {
@@ -30,17 +36,20 @@ function content() {
             <div> 
             <div class = "cute">${drink.strDrink}</div> 
              <div class = 'cuter'> ${drink.strIngredient1}, ${drink.strIngredient2}, ${drink.strIngredient3}, ${drink.strIngredient4}, ${drink.strIngredient5}, ${drink.strIngredient6}, ${drink.strIngredient7}</div> 
-            <div class = 'cutest'>${drink.strInstructions}  <ul>Like! <span class="like-glyph">&#x2661;</span></ul></div> 
+            <div class = 'cutest'>${drink.strInstructions}    </div> 
+
+         <ul><span class="like-glyph">&#x2661;</span></ul>
             </div>`
              
             
             article.append(div)
               document.querySelector('.card')
               const hide = document.querySelectorAll('.cuter')
+              
 
-                // hide.forEach(item => console.log((item.innerHTML).split(',')))
+                //  hide.forEach(item => console.log((item.innerHTML).split(',')))
 
-                // hide.forEach(item => console.log((item.innerHTML).split(',').filter(item => { if (item != " null") { console.log(item)}}))) 
+                //  hide.forEach(item => console.log((item.innerHTML).split(',').filter(item => { if (item != " null") { console.log(item)}}))) 
     
                })
       
@@ -68,19 +77,7 @@ function content() {
         }
         function out(b) {
           b.target.parentNode.remove()
-        }
-
-        
-        
-
-        let likes = document.querySelectorAll('.like-glyph')
-
-        
-        document.querySelectorAll('.like-glyph').liking();
-        likes.liking();
-
-        
-      
+        }     
         
 }
 
